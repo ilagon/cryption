4a07c09cb3ec90378cb2f03fb36b1e4bc91594e6
Lorenzo <lorenzyadrian@gmail.com>
2021-08-09 00:16:50 +0800

    Project Cleanup

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
 
-var indexRouter = require('./routes/index');
 let cryptoRouter = require('./routes/coingecko');
 let notionRoute = require('./routes/notion');
 
@@ -14,7 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
 app.use('/test', cryptoRouter);
 app.use('/v1/notion', notionRoute);
 
