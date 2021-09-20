c51730349d2dab0b4e5878c03c1d1e353bed2968
Lorenzo <lorenzyadrian@gmail.com>
2021-09-20 20:59:44 +0800

    Integrate stock update api

--- controller/FinnhubController.js
+++ controller/FinnhubController.js
@@ -8,7 +8,38 @@ const findTrackingList = async () => {
       database_id: dbID,
     });
     return response.results;
+    //response.results[0].properties["Ticker Symbol"].title[0].text.content
   };
 
+const getPrice = async(ticker) => {
+  const response = await axios.get(`https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${process.env.FINNHUB_API_KEY}`)
+  .then(res => res.data)
+  .catch(err => console.log(err));
+  return response;
+}
+
+exports.updateStockPrices = async (req, res) => {
+  try {
+    const trackingList = await findTrackingList();
+    trackingList.forEach(async tracker => {
+      const newPrice = await getPrice(tracker.properties["Ticker Symbol"].title[0].text.content);
+      await notion.pages.update({
+        page_id: tracker.id,
+        properties: {
+          "Current Price": {
+            number: newPrice.c
+          }
+        }
+      })
+    });
+    res.status(200).json({
+      message: "Update Completed.",
+    });
+  } catch (err) {
+    res.status.json({
+      error: err,
+    });
+  }
+};
 
 
--- routes/notion.js
+++ routes/notion.js
@@ -4,6 +4,6 @@ const coingeckoController = require('../controller/CoingeckoController');
 const finnhubController = require('../controller/FinnhubController');
 
 router.get("/crypto", coingeckoController.updateTrackedPrices);
-
+router.get("/stocks", finnhubController.updateStockPrices);
 
 module.exports = router;
